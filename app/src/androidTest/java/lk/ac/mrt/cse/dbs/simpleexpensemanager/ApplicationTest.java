b0c81029f8b893ab55d82a0f136a08a5245a0c12
springyboiii <sugumararvinth19991018@gmail.com>
2022-05-10 11:27:14 +0530

    Unit test

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,18 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+//import androidx.test.core.app.ApplicationProvider;
+import static org.junit.Assert.*;
+import android.app.Activity;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistantExpenseManager;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
+public class ApplicationTest {
+
 }
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistantExpenseManager.java
@@ -52,11 +52,11 @@ public class PersistantExpenseManager extends ExpenseManager {
         AccountDAO persistantAccountDAO = new PersistantAccountDAO(dbHandler);
         setAccountsDAO(persistantAccountDAO);
 
-        // dummy data
-//        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
-//        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-//        getAccountsDAO().addAccount(dummyAcct1);
-//        getAccountsDAO().addAccount(dummyAcct2);
+//         dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
 
 
         /*** End ***/
